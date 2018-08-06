f9a1a529140195317cb731efbf39741841cc7ca3
靳炳淑 <598029681@qq.com>
2018-08-06 17:30:21 +0800

    简易版的TextCutter 仅实现单行

--- src/components/PopularPart/index.js
+++ src/components/PopularPart/index.js
@@ -3,10 +3,23 @@ import PropTypes from "prop-types";
 import styles from "./index.module.less";
 import picture from "./data/picture.png";
 import greenPoint from "./data/greenPoint.png";
+import TextCutter from "../TextCutter";
 
 
 const PopularPartProps = {
-
+    imgSrc: PropTypes.string.isRequired,
+    top0: PropTypes.string.isRequired,
+    title0: PropTypes.string.isRequired,
+    content0: PropTypes.string.isRequired,
+    top1: PropTypes.string.isRequired,
+    title1: PropTypes.string.isRequired,
+    content1: PropTypes.string.isRequired,
+    top2: PropTypes.string.isRequired,
+    title2: PropTypes.string.isRequired,
+    content2: PropTypes.string.isRequired,
+    top3: PropTypes.string.isRequired,
+    title3: PropTypes.string.isRequired,
+    content3: PropTypes.string.isRequired,
 };
 
 const DefaultPopularPartProps = {
@@ -38,7 +51,7 @@ class PopularPart extends Component {
                     className={styles["hover-bottom"]}>
                     <div className={styles.top}>{top0}</div>
                     <div className={styles.title}>{title0}</div>
-                    <div className={styles.content}>{content0}</div>
+                    <div className={styles.content}><TextCutter maxLines={1} maxLength={60}>{content0}</TextCutter></div>
                 </div>
             </div>
 
@@ -54,25 +67,23 @@ class PopularPart extends Component {
                      onClick={() => this.handleClick(1)}>
                     <div className={styles["tip-top"]}>{top1}</div>
                     <div className={styles["tip-title"]}>{title1}</div>
-                    <div className={styles["tip-content"]}>{content1}</div>
+                    <div className={styles["tip-content"]}><TextCutter maxLines={1} maxLength={60}>{content1}</TextCutter></div>
                 </div>
                 <div className={styles["one-tip"]}
                      onClick={() => this.handleClick(2)}>
                     <div className={styles["tip-top"]}>{top2}</div>
                     <div className={styles["tip-title"]}>{title2}</div>
-                    <div className={styles["tip-content"]}>{content2}</div>
+                    <div className={styles["tip-content"]}><TextCutter maxLines={1} maxLength={60}>{content2}</TextCutter></div>
                 </div>
                 <div className={styles["one-tip"]}
                      onClick={() => this.handleClick(3)}>
                     <div className={styles["tip-top"]}>{top3}</div>
                     <div className={styles["tip-title"]}>{title3}</div>
-                    <div className={styles["tip-content"]}>{content3}</div>
+                    <div className={styles["tip-content"]}><TextCutter maxLines={1} maxLength={60}>{content3}</TextCutter></div>
                 </div>
 
             </div>
-        </div>;
-    }
-}
+        </di
 
 PopularPart.propTypes = PopularPartProps;
 
