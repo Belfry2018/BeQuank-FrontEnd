5e2ac9582a7dc5ad1abb2e3fcd7add3498969a6c
靳炳淑 <598029681@qq.com>
2018-08-07 16:46:13 +0800

    修改了自适应

--- src/components/PopularPart/index.js
+++ src/components/PopularPart/index.js
@@ -39,51 +39,56 @@ class PopularPart extends Component {
     render() {
        const {imgSrc, top0, title0, content0, top1, title1, content1, top2, title2, content2, top3, title3, content3 } = this.props;
 
-        return <div
-            className={styles["popular-part"]}>
-            <div
-                className={styles["left-picture-part"]}>
-                <img
-                    className={styles["picture"]}
-                    src={picture}>
-                </img>
-                <div
-                    className={styles["hover-bottom"]}>
-                    <div className={styles.top}>{top0}</div>
-                    <div className={styles.title}>{title0}</div>
-                    <div className={styles.content}><TextCutter maxLines={1} maxLength={60}>{content0}</TextCutter></div>
-                </div>
-            </div>
+        return(
+            <div className={styles["popular-part"]}>
+                    <div className={styles["left-picture-part"]}>
+                         <div className={styles.picture}>
+                             <img src={picture} />
+                         </div>
+
+                     <div className={styles["hover-bottom"]}>
+                       <div className={styles.top}>{top0}</div>
+                       <div className={styles.title}>{title0}</div>
+                       <div className={styles.content}>
+                          <TextCutter maxLines={1} maxLength={60}>{content0}</TextCutter>
+                       </div>
+                     </div>
+                    </div>
 
             <div className={styles["right-text-part"]}>
                 <div className={styles["title"]}>
-                    <img
-                        className={styles["title-picture"]}
-                        src={greenPoint}></img>
+                    <img className={styles["title-picture"]} src={greenPoint}></img>
                     <div className={styles["title-text"]}>What you might like</div>
                 </div>
 
                 <div className={styles["one-tip"]}
-                     onClick={() => this.handleClick(1)}>
+                     onClick={() => this.handleClick(1)}
+                >
                     <div className={styles["tip-top"]}>{top1}</div>
                     <div className={styles["tip-title"]}>{title1}</div>
-                    <div className={styles["tip-content"]}><TextCutter maxLines={1} maxLength={60}>{content1}</TextCutter></div>
+                    <div className={styles["tip-content"]}>
+                        <TextCutter maxLines={1} maxLength={60}>{content1}</TextCutter>
+                    </div>
                 </div>
                 <div className={styles["one-tip"]}
                      onClick={() => this.handleClick(2)}>
                     <div className={styles["tip-top"]}>{top2}</div>
                     <div className={styles["tip-title"]}>{title2}</div>
-                    <div className={styles["tip-content"]}><TextCutter maxLines={1} maxLength={60}>{content2}</TextCutter></div>
+                    <div className={styles["tip-content"]}>
+                        <TextCutter maxLines={1} maxLength={60}>{content2}</TextCutter>
+                    </div>
                 </div>
                 <div className={styles["one-tip"]}
                      onClick={() => this.handleClick(3)}>
                     <div className={styles["tip-top"]}>{top3}</div>
                     <div className={styles["tip-title"]}>{title3}</div>
-                    <div className={styles["tip-content"]}><TextCutter maxLines={1} maxLength={60}>{content3}</TextCutter></div>
+                    <div className={styles["tip-content"]}>
+                        <TextCutter maxLines={1} maxLength={60}>{content3}</TextCutter>
+                    </div>
                 </div>
-
             </div>
-        </div>;
+        </div>
+    );
     }
 }
 
