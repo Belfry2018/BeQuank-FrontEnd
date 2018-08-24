273c89023196f7fc3ae74c337244764423a0557e
kunduin <15852158126@163.com>
2018-08-24 23:00:53 +0800

    补救merge引起的错误

--- src/services/apiCourse.js
+++ src/services/apiCourse.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import {version} from "./apiOverview";
-=======
-import { version } from "./apiOverview";
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
 import request from "../utils/request";
 
 /**
  * 发布教程
  * @param params
-<<<<<<< HEAD
  * @return {Object}
  * @example
  * pushTutorial({
-=======
- * @returns {Object}
- * @example
- * postTutorial({
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
  *  authorNickname:"jack",
  *  authorId:"abc",
  *  title:"how to make money",
@@ -26,7 +16,6 @@ import request from "../utils/request";
  *  content:"money is very important",
  *  time:"2018-08-13"
  * })
-<<<<<<< HEAD
  * @throws 418
  */
 export const pushTutorial = params => {
@@ -34,34 +23,18 @@ export const pushTutorial = params => {
     method: "POST",
     body: params
   });
-=======
- * @throws
- */
-export const postTutorial = params => {
-    return request(`${version}/tutorial`, {
-        method: "POST",
-        body: params
-    });
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
 };
 
 /**
  * 根据条件筛选教程
  * @param params
-<<<<<<< HEAD
  * @return {Object}
  * @example
  * getTutorials({
-=======
- * @returns {Object}
- * @example
- * findTutorials({
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
  *  authorId:"jack",
  *  publishTime:"2018-08-13",
  *  cover:"cover",
  *  abstract:"blabla",
-<<<<<<< HEAD
  *  keyWords:{"money", "fortune"},
  *  tutorialType:"BASIC"
  * })
@@ -85,42 +58,13 @@ export const getTutorials = params => {
 export const getTheTutorial = tutorialId => {
   return request(`${version}/tutorial?id=${tutorialId}`)
 };
-=======
- *  keyWords:{"money", "fortune"}
- * })
- */
-export const findTutorials = params =>{
-    return request(`${version}/tutorials`, {
-        method: "POST",
-        body: params
-    });
-}
-
-/**
- * 查看具体教程
- * @param id
- * @returns {Object}
- */
-export const showTutorial = id => {
-    return request(`${version}/tutorial`, {
-        method: "GET",
-        body: { id }
-    });
-}
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
 
 /**
  * 发表评论
  * @param params
-<<<<<<< HEAD
  * @return {Object}
  * @example
  * postComment({
-=======
- * @returns {Object}
- * @example
- * makeComment({
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
  *  tutorialId:"tutorialA",
  *  content:"lalalala",
  *  writerNickname:"tom",
@@ -128,7 +72,6 @@ export const showTutorial = id => {
  *  time:"2018-08-13"
  * })
  */
-<<<<<<< HEAD
 export const postComment = params => {
   return request(`${version}/tutorials`, {
     method: "POST",
@@ -136,23 +79,11 @@ export const postComment = params => {
   });
 };
 
-=======
-export const makeComment = params => {
-    return request(`${version}/comment`, {
-        method: "POST",
-        body: params
-    });
-}
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
 
 /**
  * 回复评论
  * @param params
-<<<<<<< HEAD
  * @return {Object}
-=======
- * @returns {Object}
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
  * @example
  * replyComment({
  *  commentId:"comment2",
@@ -163,37 +94,22 @@ export const makeComment = params => {
  * })
  */
 export const replyComment = params => {
-<<<<<<< HEAD
   return request(`${version}/reply`, {
     method: "POST",
     body: params
   });
 };
-=======
-    return request(`${version}/reply`, {
-        method: "POST",
-        body: params
-    });
-}
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
 
 /**
  * 点赞教程（可取消）
  * @param params
-<<<<<<< HEAD
  * @return {Object}
  * @example
  * likeComment({
-=======
- * @returns {Object}
- * @example
- * likeTutorial({
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
  *  tutorialId:"tutorialA",
  *  likerId:"nancy"
  * })
  */
-<<<<<<< HEAD
 export const likeComment = params =>{
   return request(`${version}/like/tutorial`,{
     method:"POST",
@@ -201,22 +117,13 @@ export const likeComment = params =>{
   })
 };
 
-=======
-export const likeTutorial = params => {
-    return request(`${version}/like/tutorial`, {
-        method: "POST",
-        body: params
-    });
-}
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
 
 /**
  * 点赞评论（可取消）
  * @param params
-<<<<<<< HEAD
  * @return {Object}
  * likeComment({
- *  commentId:"tutorialA",
+ *  tutorialId:"tutorialA",
  *  likerId:"nancy"
  * })
  */
@@ -226,18 +133,3 @@ export const likeReply = params =>{
     body:params
   })
 };
-=======
- * @returns {Object}
- * @example
- * likeTutorial({
- *  commentId:"commentA",
- *  likerId:"nancy"
- * })
- */
-export const likeComment = params => {
-    return request(`${version}/like/comment`, {
-        method: "POST",
-        body: params
-    });
-}
->>>>>>> 71179c8c9b7e6582356474132f59b18978411984
