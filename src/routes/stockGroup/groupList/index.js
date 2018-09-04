b03755805ac52b7cc5b26d075a883a040d579249
Unknown <598029681@qq.com>
2018-09-04 19:02:09 +0800

    save

---
+++ src/routes/stockGroup/groupList/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Form} from "antd/lib/index";
+import {withRouter} from "react-router-dom";
+import styles from "./index.module.less";
+import RecordList from "../../../components/RecordList/index";
+import NameCard from "../../../components/NameCard/index";
+
+class GroupList extends React.Component {
+    render () {
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default withRouter(GroupList);
