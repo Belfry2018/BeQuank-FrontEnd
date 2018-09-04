a1f883e4eeac68d701db016791333e413ff544a6
Unknown <598029681@qq.com>
2018-09-05 07:46:05 +0800

    small changes

--- src/components/RecordList/index.js
+++ src/components/RecordList/index.js
@@ -5,6 +5,11 @@ import Tip from "./Tip/index";
 import SmallPoint from "../SmallPoint";
 
 class RecordList extends Component {
+
+    onRowClicked = (id) => {
+        this.props.history.push(`/record/${id}`);
+    }
+
     render() {
         const { params } = this.props;
 
@@ -13,13 +18,13 @@ class RecordList extends Component {
                 <div className={styles.title}>
                     <div className={styles.pp}>
                         <div className={styles.point}><SmallPoint/></div>
-                        <div className={styles.text}>我的自选股组</div>
+                        <div className={styles.text}>策略收藏</div>
                     </div>
                 </div>
                 <div className={styles["tip-content"]}>
                     {
                         params.map((item, index)=>{
-                            return <Tip title={item.recordName} time={item.recordTime}></Tip>
+                            return <Tip  title={item.recordName} time={item.recordTime}></Tip>
                         })
                     }
                 </div>
