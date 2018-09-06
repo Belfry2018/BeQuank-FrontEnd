173010156f1fa2fd8125c99d48423d55560e3e8a
kunduin <15852158126@163.com>
2018-09-06 19:33:26 +0800

    完成删除逻辑

--- src/components/RecordList/Tip/index.js
+++ src/components/RecordList/Tip/index.js
@@ -1,28 +1,48 @@
-import React, { Component, } from "react";
-import { Button } from 'antd';
+import React, { Component } from "react";
+import { Button, Popconfirm, message } from "antd";
 import styles from "./index.module.less";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
+import { deleteRecord } from "../../../services/apiStrategy";
 
 class Tip extends Component {
-    render() {
-        const { title, time, id } = this.props;
+  onDeleteEvent = async () => {
+    await deleteRecord(this.props.id);
+    this.props.history.push({
+      pathname: `/groupList`,
+    });
+    message.success("已成功删除");
+  };
 
-        return (
-          <Link to={`/groupList/${id}`}>
-            <div className={styles.main}>
-                <div className={styles["top-part"]}>
-                    <div className={styles.title}>{title}</div>
-                    <div className={styles.button}>
-                        <Button type={"dashed"} size={"small"} >删除</Button>
-                    </div>
-                </div>
-                <div className={styles["bottom-part"]}>
-                    <div className={styles.rooter}>创建时间: {time}</div>
-                </div>
+  render() {
+    const { title, time, id } = this.props;
+
+    return (
+      <div className={styles.wholePart}>
+        <Link to={`/groupList/${id}`}>
+          <div className={styles.main}>
+            <div className={styles["top-part"]}>
+              <div className={styles.title}>{title}</div>
             </div>
-          </Link>
-        );
-    }
+            <div className={styles["bottom-part"]}>
+              <div className={styles.rooter}>创建时间: {time}</div>
+            </div>
+          </div>
+        </Link>
+        <div className={styles.button}>
+          <Popconfirm
+            title={`你确定要删除组选股组「${title}」吗`}
+            okText="是的"
+            cancelText="不了"
+            onConfirm={this.onDeleteEvent}
+          >
+            <Button type={"dashed"} size={"small"}>
+              删除
+            </Button>
+          </Popconfirm>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Tip;
+export default withRouter(Tip) ;
--- src/components/RecordList/index.js
+++ src/components/RecordList/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import styles from "./index.module.less";
 import Tip from "./Tip/index";
-import Empty from "../Empty";
 
 class RecordList extends Component {
   render() {
@@ -10,7 +9,7 @@ class RecordList extends Component {
     return (
       <div className={styles["tip-content"]}>
         {params.map((item, index) => {
-          return <Tip title={item.recordName} time={item.recordTime} id={item.recordId} />;
+          return <Tip key={`recordListItem${index}`} title={item.recordName} time={item.recordTime} id={item.recordId} />;
         })}
       </div>
     );
