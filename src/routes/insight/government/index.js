4aea832b47e7ec34184f5c87867b6aa440e9e5fc
kunduin <15852158126@163.com>
2018-09-07 17:01:02 +0800

    计划government架构

--- src/routes/insight/government/index.js
+++ src/routes/insight/government/index.js
@@ -1,11 +1,22 @@
 import React, { PureComponent } from "react";
-import NavBar from "../components/NavBar"
-import Styles from "../index.module.less"
+import NavBar from "../components/NavBar";
+import Styles from "../index.module.less";
+import SmallPoint from "../../../components/SmallPoint";
 
 export default class GovernmentInsight extends PureComponent {
   render() {
-    return <div className={Styles.bodySection}>
-      <NavBar/>
-    </div>;
+    return (
+      <div className={Styles.bodySection}>
+        <NavBar />
+        <div className={Styles.government}>
+          <div className={Styles.title}>
+            <SmallPoint title={"政府文章"} />
+          </div>
+          <div>
+            <
+          </div>
+        </div>
+      </div>
+    );
   }
 }
