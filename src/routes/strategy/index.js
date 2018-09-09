a12dbe68852398f99d6110871c2c37e9b16611c2
Unknown <598029681@qq.com>
2018-09-09 20:38:14 +0800

    修改问卷

--- src/routes/strategy/index.js
+++ src/routes/strategy/index.js
@@ -3,10 +3,18 @@ import Styles from "./index.module.less";
 import Question from "../../components/Question";
 import { makeAnswer } from "../../services/apiStrategy";
 import SmallPoint from "../../components/SmallPoint";
+import Part2 from "../../components/Question/Part2/index";
 import {withRouter} from "react-router-dom";
+import {message} from "antd/lib/index";
+import {setUserProfile} from "../../services/apiUser";
 
 class Strategy extends PureComponent {
-  handleFormSubmit = async values => {
+
+  state = {
+    step: 0
+  };
+
+  handleFormSubmit1 = async values => {
     const result= await makeAnswer(values);
     this.props.history.push({
       pathname: '/strategy/result',
@@ -14,13 +22,30 @@ class Strategy extends PureComponent {
     });
   };
 
+    handleSubmitPart2 = async values => {
+      try {
+            setUserProfile(values);
+            message.success("提交成功！");
+      } catch (e) {
+            let errorMessage = "";
+            if (e.name === 418) {
+                errorMessage = "xxx";
+            }
+      }
+      setTimeout(this.setState({
+          step: 1
+      }), 1000)
+    }
+
   render() {
     return (
       <div className={Styles.section}>
         <div className={Styles.item}>
-          <SmallPoint title={"策略问卷"} />
           <div style={{height:30}} />
-          <Question onSubmit={this.handleFormSubmit} />
+            {
+                { 0:<Part2 onSubmit={this.handleSubmitPart2}/>,
+                  1:<Question onSubmit={this.handleFormSubmit1} />}[this.state.step]
+            }
         </div>
       </div>
     );
