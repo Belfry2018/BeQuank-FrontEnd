6da80aea85ee17b492c5c277645670dae18fe665
gabriel <838492657@qq.com>
2018-10-21 11:40:37 +0800

    付费提问组件完成

---
+++ src/components/PayforQuestion/piece/index.js
@@ -0,0 +1,40 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import styles from "./index.module.less";
+import { Avatar } from 'antd';
+import { Icon } from 'antd';
+
+const PieceProperty = {
+  /** Define avatar'url of button */
+  avatar: PropTypes.string.isRequired,
+  /** Define bio of button */
+  bio: PropTypes.string.isRequired,
+  /** Define username of button */
+  username: PropTypes.string.isRequired,
+};
+
+class Piece extends PureComponent {
+  render() {
+    const { avatar, bio, username } = this.props;
+    return (
+      <div className={styles.piece}>
+        <div className={styles.avatar}>
+          <Avatar size={56} src={avatar} />
+        </div>
+        <div className={styles.right}>
+          <div className={styles.namecontent}>
+            <div className={styles.username}>{username}</div>
+            <div className={styles.content}>{bio}</div>
+          </div>
+          <div className={styles.icon}>
+            <Icon type="right" theme="outlined" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Piece.propTypes = PieceProperty;
+
+export default Piece;
